e602999723f8ac51df7bda207628158b09c0e596
Nutan-Meher24 <nutanbot@gmail.com>
2025-03-23 22:21:28 +0530

    Add files via upload

---
+++ day1.js
@@ -0,0 +1,38 @@
+
+// #VARIABLES, CONSTANTS
+
+console.log("hello nutan");
+
+const accountId= 214
+let accountEmail= "nutanbot@gmail.com"
+var accountPassword= "37653274hdsdh"
+accountCity= "JNG"
+
+// this & (/*   */) is a comment symbol in js.
+
+/* 
+prefer not to use var because of issue in block scope & functional scope 
+*/
+
+console.table({accountId, accountEmail, accountPassword, accountCity})
+
+//#DATA TYPES
+
+"use strict"; // treat all js code as newer version 
+
+
+let name= "Nutan"   //string
+let age= "21"    //number
+let IsLoggedIn= false    //boolean
+
+
+console.log(typeof "nutan")
+console.log(typeof 21)
+console.log(typeof false)
+console.log(typeof undefined)
+
+
+
+
+
+
